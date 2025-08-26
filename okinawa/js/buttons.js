49a7cc4af3fa428e49671df07fb7e82792b29931
Ilia Chebanik <iliebanik@gmail.com>
2025-08-26 09:59:39 +0300

    Refactor

--- okinawa/js/buttons.js
+++ okinawa/js/buttons.js
@@ -1,17 +1,15 @@
-
-
 let isUserLoginDialogOpen = false;
-document.getElementById('header-user-button').addEventListener('click', function() {
-    if (isUserLoginDialogOpen) {
-        document.getElementById('user-login-dialog').close();
-        isUserLoginDialogOpen = false;
-    } else {
-        document.getElementById('user-login-dialog').showModal();
-        isUserLoginDialogOpen = true;
-    }
-});
-
-document.getElementById('close-user-login-dialog-button').addEventListener('click', function() {
+document.getElementById('header-user-button').addEventListener('click', function () {
+  if (isUserLoginDialogOpen) {
     document.getElementById('user-login-dialog').close();
     isUserLoginDialogOpen = false;
+  } else {
+    document.getElementById('user-login-dialog').showModal();
+    isUserLoginDialogOpen = true;
+  }
+});
+
+document.getElementById('close-user-login-dialog-button').addEventListener('click', function () {
+  document.getElementById('user-login-dialog').close();
+  isUserLoginDialogOpen = false;
 });
